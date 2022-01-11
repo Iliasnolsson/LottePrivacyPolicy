341169872a313f51ce93a5790bea1c87a3b6543d
Iliasnolsson <58471856+Iliasnolsson@users.noreply.github.com>
2022-01-11 18:58:04 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,12 +8,12 @@ export default function Home() {
     
       <Head>
         <title>Lotte Calculator</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/LotteIco.ico" />
       </Head>
     
       <main>
-        <Header title="Lotte Calculator Privacy Policy" />
-        <p className="description">
+        <Header title="Lotte Calculator \n Privacy Policy" />
+        <p className="description" style="text-align:center;">
           We do not store any of your data. Lotte Calculator and all of our other apps do not collect or store any of your data.
         </p>
       </main>
