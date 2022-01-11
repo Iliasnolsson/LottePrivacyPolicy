a56b6e5d1579ef92d1fba89392d9d7bd378f9e5e
Iliasnolsson <58471856+Iliasnolsson@users.noreply.github.com>
2022-01-11 20:34:43 +0100

    Add files via upload

--- pages/support.js
+++ pages/support.js
@@ -1,23 +1,23 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import React from "react";
+import styles from '../styles/Support.module.module.css'
 
 export default function Support() {
     return (
         <div className="container">
-        
           <Head>
             <title>Lotte Calculator</title>
             <link rel="icon" href="/favicon.ico" />
           </Head>
         
           <main>
-            <Header title="iliasnolsson@icloud.com" />
+            <Header className="header" title="iliasnolsson@icloud.com" />
             <p className="description">
-              Feel free to contact me with any feedback or if you need support
+              Feel free to contact me with any feedback or for support
             </p>
           </main>
-    
         </div>
       )
 }
