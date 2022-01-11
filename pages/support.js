8356e4692d190c29e9647e5ef866c5a869144e1b
Iliasnolsson <58471856+Iliasnolsson@users.noreply.github.com>
2022-01-11 20:13:42 +0100

    Add files via upload

---
+++ pages/support.js
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
+import React from "react";
+import styles from '../styles/About.module.css'
+
+export default function About() {
+    return (
+        <div className="container">
+        
+          <Head>
+            <title>Lotte Calculator</title>
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
+        
+          <main>
+            <Header title="iliasnolsson@icloud.com" />
+            <p className="description">
+              Feel free to contact me with any feedback or for support
+            </p>
+          </main>
+    
+        </div>
+      )
+}
